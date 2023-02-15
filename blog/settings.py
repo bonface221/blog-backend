c0b1b3ce0d5b5a6a96cef3705c72471a67006281
bonface221 <mainaboniface221@gmail.com>
2023-02-16 02:10:20 +0300

    deployment to railways

--- blog/settings.py
+++ blog/settings.py
@@ -133,8 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "https://web-production-4431a.up.railway.app/staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
